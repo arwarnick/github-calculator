7e7c4386ab1a30fddb319138c06e2b098f00c2cc
Austin Warnick <arwarnick@gmail.com>
2025-02-18 15:06:26 -0700

    feat: Add fibonacciNumberFinder implementation

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -41,7 +41,15 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 1) return n;
+
+        int prev = 0, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
     }
 
 
